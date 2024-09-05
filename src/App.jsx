b72851c67020d7e70d9c4bd825e2cd92503593ce
Nirush4 <nirushvj@gmail.com>
2024-09-05 11:50:22 +0200

    Welcome to the world

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and Testing Page
+          Edit <code>src/App.jsx</code> welcome to the world
         </p>
       </div>
       <p className="read-the-docs">
